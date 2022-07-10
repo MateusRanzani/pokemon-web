3ce923bf6407496116ba946d1793ce2b0c185950
MateusRanzani <mateusranzani@hotmail.com>
2022-07-10 14:24:32 -0300

    Update

--- src/Components/PageTwo/Footer/index.tsx
+++ src/Components/PageTwo/Footer/index.tsx
@@ -1,20 +1,30 @@
-
-import { Boton, BoxCircle, Container, ImgBag, ImgPokeball, ImgPokedex } from "./style";
+import {
+  Boton,
+  BoxCircle,
+  Container,
+  ImgBag,
+  ImgPokeball,
+  ImgPokedex,
+} from "./style";
 
 export function Footer() {
   return (
     <>
       <Container>
-        
         <BoxCircle>
+          <a href="">
             <ImgBag src="/assets/thisbag.png" alt="Bag" width={100} />
+          </a>
         </BoxCircle>
 
         <Boton>
-          <ImgPokeball src="/assets/Pokeboll.png" alt="Pokeboll" width={60} />
-          <ImgPokedex src="/assets/Pokedex.gif" alt="Pokeboll" width={80} />
+          <a href="">
+            <ImgPokeball src="/assets/Pokeboll.png" alt="Pokeboll" width={60} />
+          </a>
+          <a href="">
+            <ImgPokedex src="/assets/Pokedex.gif" alt="Pokeboll" width={80} />
+          </a>
         </Boton>
-        
       </Container>
     </>
   );
